c414e907d28fa0a37df8058e2ea6605005ce9ed5
EinLinuus <github@linusbenkner.de>
2021-12-15 18:52:00 +0100

    Prettier

--- src/index.js
+++ src/index.js
@@ -2,67 +2,70 @@ const plugin = require("tailwindcss/plugin")
 
 let pseudoVariants = [
     // Positional
-    ['first', ':first-child'],
-    ['last', ':last-child'],
-    ['only', ':only-child'],
-    ['odd', ':nth-child(odd)'],
-    ['even', ':nth-child(even)'],
-    'first-of-type',
-    'last-of-type',
-    'only-of-type',
+    ["first", ":first-child"],
+    ["last", ":last-child"],
+    ["only", ":only-child"],
+    ["odd", ":nth-child(odd)"],
+    ["even", ":nth-child(even)"],
+    "first-of-type",
+    "last-of-type",
+    "only-of-type",
 
     // State
-    'visited',
-    'target',
-    ['open', '[open]'],
+    "visited",
+    "target",
+    ["open", "[open]"],
 
     // Forms
-    'default',
-    'checked',
-    'indeterminate',
-    'placeholder-shown',
-    'autofill',
-    'required',
-    'valid',
-    'invalid',
-    'in-range',
-    'out-of-range',
-    'read-only',
+    "default",
+    "checked",
+    "indeterminate",
+    "placeholder-shown",
+    "autofill",
+    "required",
+    "valid",
+    "invalid",
+    "in-range",
+    "out-of-range",
+    "read-only",
 
     // Content
-    'empty',
+    "empty",
 
     // Interactive
-    'focus-within',
-    'hover',
-    'focus',
-    'focus-visible',
-    'active',
-    'disabled',
+    "focus-within",
+    "hover",
+    "focus",
+    "focus-visible",
+    "active",
+    "disabled",
 ].map((variant) => (Array.isArray(variant) ? variant : [variant, `:${variant}`]))
 
 module.exports = plugin.withOptions(function (options) {
     const customGroups = options?.groups || []
-    return function({ addVariant, e }) {
-
+    return function ({ addVariant, e }) {
         for (let [variantName, state] of pseudoVariants) {
             addIt("scoped")
-            customGroups.forEach(customGroup => addIt(customGroup))
+            customGroups.forEach((customGroup) => addIt(customGroup))
 
             function addIt(groupName) {
                 // * New addVariant API (TailwindCSS 3.x)
                 // addVariant(`group-${groupName}-${variantName}`, [`:merge(.group-${groupName})${state} > &`, `:merge(.group-${groupName})${state} *:not(.group-${groupName}) &`])
 
                 // * Old addVariant API (TailwindCSS 1.x & 2.x)
-                addVariant( `group-${groupName}-${variantName}`, ({ modifySelectors, separator }) => {
-                    modifySelectors(({ className }) => {
-                        let groupSelector = `group-${groupName}${state}`
-                        let itemSelector = e(`group-${groupName}-${variantName}${separator}${className}`)
-                        return `.${groupSelector} > .${itemSelector}, .${groupSelector} *:not(.group-${groupName}) .${itemSelector}`
-                    })
-                })
+                addVariant(
+                    `group-${groupName}-${variantName}`,
+                    ({ modifySelectors, separator }) => {
+                        modifySelectors(({ className }) => {
+                            let groupSelector = `group-${groupName}${state}`
+                            let itemSelector = e(
+                                `group-${groupName}-${variantName}${separator}${className}`
+                            )
+                            return `.${groupSelector} > .${itemSelector}, .${groupSelector} *:not(.group-${groupName}) .${itemSelector}`
+                        })
+                    }
+                )
             }
         }
-
     }
 })
